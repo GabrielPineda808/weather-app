b0ac0723f6c8ac30675a1f8e08d62e4e2737f161
Gabriel Pineda <gabepineda6@gmail.com>
2020-09-29 23:03:14 -0700

    created another ajax call to pull forecast fure info in order ot append that info to the forecast cards

--- script.js
+++ script.js
@@ -16,6 +16,27 @@ $(document).ready(function(){
 
               $("#history").append($("<button>").text(response.name).attr("class", "list-group-item"))
         });
+        forecast(searchInput)
     });
+
+    function forecast(searchInput){
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&cnt=40&appid=0f648c83a8f1c0908eb37a9e5dfe1a58&units=Imperial";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            $("#temp1").text(response.list[5].main.temp); 
+            $("#temp2").text(response.list[14].main.temp); 
+            $("#temp3").text(response.list[21].main.temp); 
+            $("#temp4").text(response.list[29].main.temp); 
+            $("#temp5").text(response.list[38].main.temp); 
+        
+            $("#humid1").text(response.list[5].main.humidity);
+            $("#humid2").text(response.list[14].main.humidity);
+            $("#humid3").text(response.list[21].main.humidity);
+            $("#humid4").text(response.list[29].main.humidity);
+            $("#humid5").text(response.list[38].main.humidity);  
+        });
+    }
         
 });
