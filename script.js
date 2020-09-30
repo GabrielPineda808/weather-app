8d9fe779510778b4f700c8be3ec9ace77271d569
Gabriel Pineda <gabepineda6@gmail.com>
2020-09-29 21:40:56 -0700

    added the proper text to correspond to each section with the updated id tags

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 $(document).ready(function(){
     var searchBtn = $("#searchBtn");
     searchBtn.on("click", function(event){       
-        var searchInput = $("#search").val();
-        var wCard = $("#wCard");
-        var cCard = $("#cCard");
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+searchInput+"&appid=b107c8b9bc4a0c422d2aa876058229a3&units=imperial"
         event.preventDefault();
+        var searchInput = $("#search").val();
+        console.log(searchInput)
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+searchInput+"&appid=0f648c83a8f1c0908eb37a9e5dfe1a58&units=imperial"
         $.ajax({
             url: queryURL,
             method: "GET"
-          }).then(function(response) {
-            console.log(response);
-            var newDiv = $("<div>");
-            var htag = $("<h1>");
-            var ptag = $("<p>");
-            htag.append(response.name);
-            newDiv.append(htag);
-            wCard.append(newDiv);
-
-            ptag.append("Temperature: "+response.main.temp + "°F");
-            newDiv.append(ptag);
-            wCard.append(newDiv);
-            var ptag = $("<p>");
-            ptag.append("Humidity: "+response.main.humidity+"%");
-            newDiv.append(ptag);
-            wCard.append(newDiv);
-            var ptag = $("<p>");
-            ptag.append("Wind Speed: "+response.wind.speed+ "MPH");
-            newDiv.append(ptag);
-            wCard.append(newDiv);
-            console.log(response.main.temp)
+          }).then(function(response){
+              $("#city").text(response.name)
+              $("#temp").text(response.main.temp)
+              $("#wind").text(response.wind.speed)
+              $("#humid").text(response.main.humidity)
         });
     });
         
